826b999fcb0db6bfe7be7c5d68991b7b8691e620
Agustín Minetti <minettiagus2@gmail.com>
2025-08-21 22:53:25 -0300

    movimiento Player1

---
+++ scripts/player_1.gd
@@ -0,0 +1,7 @@
+extends CharacterBody2D
+@export var speed: float = 500
+
+func _physics_process(delta: float) -> void:
+	var input_vector = Input.get_vector("p1_left", "p1_right", "p1_up", "p1_down")
+	velocity = input_vector * speed
+	move_and_slide()
