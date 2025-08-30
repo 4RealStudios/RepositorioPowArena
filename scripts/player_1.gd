26d9f9a90a0c0c1bb3da4691746c74bd494100f2
Agustín Minetti <minettiagus2@gmail.com>
2025-08-30 13:47:31 -0300

    BLOQUEO
    agregue mecanica de bloqueo. Los cuando el p2 pierde todas las vidas explota el juego y la hitbox de los personajes no queda rotada cuando esta quieto en una direccion que no sea la defaul

--- scripts/player_1.gd
+++ scripts/player_1.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 @export var speed: float = 85 #VELOCIDAD DEL PERSONAJE
 @export var BULLET = preload("res://scenes/bullet.tscn")
 @onready var shooting_point: Marker2D = $ShootingPointP1
-@export var player_id: int = 1 #1 o 2
+@export var player_id: int = 1 
 @export var dash_speed: float = 200 #VELOCIDAD DEL DASH
 @export var dash_duration: float = 0.2 #DURACION DEL DASH
 @export var dash_cooldown: float = 1.5 #COOLDOWN DEL DASH
@@ -16,15 +16,30 @@ var aim_dir: Vector2 = Vector2.RIGHT
 var is_dashing: bool = false  #VARIABLE PARA EL DASH
 var dash_timer: float = 0.0 #VARIABLE PARA EL COOLDOWN DEL DASH
 var dash_cooldown_timer: float = 0.0  #VARIABLE PARA EL COOLDOWN DEL DASH
+var is_locking: bool = false
+var move_input: Vector2 = Vector2.ZERO
+
+func _ready() -> void:
+	spawn_position = global_position #guarda el punto de spawn inicial
+	shoot_local_offset = shooting_point.position
 
 func _physics_process(delta: float) -> void:
 	input_vector.x = Input.get_action_strength("p1_right") - Input.get_action_strength("p1_left")
 	input_vector.y = Input.get_action_strength("p1_down") - Input.get_action_strength("p1_up")
 	input_vector = input_vector.normalized()
-	if input_vector != Vector2.ZERO:
-		aim_dir = input_vector
-	velocity = input_vector * speed
-	if input_vector != Vector2.ZERO: #ROTACION DEL SPRITE SEGUN A DONDE SE MUEVE
+	var is_locking = Input.is_action_pressed("p1_block")
+	if not is_locking:
+		if input_vector != Vector2.ZERO:
+			aim_dir = input_vector
+		velocity = input_vector * speed
+	else:
+		velocity = Vector2.ZERO
+		if input_vector != Vector2.ZERO:
+			aim_dir = input_vector
+	#if input_vector != Vector2.ZERO:
+		#aim_dir = input_vector
+	#velocity = input_vector * speed
+	if aim_dir != Vector2.ZERO: #ROTACION DEL SPRITE SEGUN A DONDE SE MUEVE
 		$Player1Sprite2D.rotation = aim_dir.angle() - PI/90
 	if dash_cooldown_timer > 0: #COOLDOWN DEL DASH
 		dash_cooldown_timer -= delta
@@ -35,16 +50,13 @@ func _physics_process(delta: float) -> void:
 			is_dashing = false
 			dash_cooldown_timer = dash_cooldown
 	else:
-		velocity = input_vector * speed
+		if not is_locking:
+			velocity = input_vector * speed
 		if Input.is_action_just_pressed("p1_dash") and dash_cooldown_timer <= 0 and input_vector != Vector2.ZERO: #DASH
 			start_dash()
 	move_and_slide()
 	if Input.is_action_just_pressed("p1_shoot"): #DISPARO
 		shoot()
-
-func _ready() -> void:
-	spawn_position = global_position #guarda el punto de spawn inicial
-	shoot_local_offset = shooting_point.position
 
 func take_damage():
 	lives -= 1
